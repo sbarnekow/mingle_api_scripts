e7606ca4e21f38f0b98ba36040491ed18c87fde5
Sarah Barnekow <mellamopepe@gmail.com>
2014-10-31 21:57:39 -0700

    implemented nokogiri with kieras help and can now update SOME ids

--- update_all_users_permissions.rb
+++ update_all_users_permissions.rb
@@ -5,10 +5,10 @@ require 'api-auth'
 require 'json'
 require 'nokogiri'
 
-all_users_url = 'https://sarahh.mingle-api.thoughtworks.com/api/v2/users.xml'
-keys = {:access_key_id => 'sbarnek_hello', :access_secret_key => 'BXXorZ+L+WmijXMSVwMtZBH2OnqQn/bFAVbeluN+IVY='}
+all_users_url = 'https://<instance name>.mingle-api.thoughtworks.com/api/v2/users.xml'
+keys = {:access_key_id => '<MINGLE USERNAME>', :access_secret_key => '<MINGLE HMAC KEY>'}
 
-def make_request(url, method, params, keys={})
+def http_get(url, keys={})
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
     
@@ -19,28 +19,15 @@ def make_request(url, method, params, keys={})
       end
     end
 
-    if method == 'get'
-      request = Net::HTTP::Get.new(uri.request_uri)
-      response = http.request(request)
-
-      all_projects = response.body
-
-      output = File.open("users.xml", "w")
-      output << all_projects
-      output.close
-    
-    elsif method == 'put'
-      body = params.to_json
-      request = Net::HTTP::Put.new(uri.request_uri)
-      request.body = body
-    
-    end
-
+    request = Net::HTTP::Get.new(uri.request_uri)
     response = http.request(request)
+    all_projects = response.body
 
     if keys[:access_key_id]
       ApiAuth.sign!(request, keys[:access_key_id], keys[:access_secret_key])
     end
+
+    response = http.request(request)
 
     if response.code.to_i > 300
         raise StandardError, <<-ERROR
@@ -51,27 +38,72 @@ def make_request(url, method, params, keys={})
         Response Body: #{response.body}
         ERROR
     end
+
+    return response
 end
 
-def count_user_objects
-  users_document = Nokogiri::XML(open('users.xml')) { |c| c.noblanks } 
-
-  return users_document.root.children.count
+def parse(response)
+  xml = Nokogiri::XML(response.body)
+  user_ids = []
+  xml.css('user').each {|user| user_ids << user.at_css('id').content }
+  return user_ids.sort!
 end
 
-def update_each_user(keys)
-    url = 'https://sarahh.mingle-api.thoughtworks.com/api/v2/users/21.xml'
-    params = {
-      :user => {
-        :name => 'sarah_changed',
-        :activated => 'false'
-      }
-    }
-    puts url
-    puts params
-    puts keys
-    p make_request(url, 'put', params, keys)
-  
+def http_put(user_array)
+  user_array.delete_if{|x| x == "1"}
+
+  user_array.each do |user|
+    url = 'https://<instance name>.mingle-api.thoughtworks.com/api/v2/' + user.to_i + '.xml'
+    keys = {:access_key_id => '<MINGLE USERNAME>', :access_secret_key => '<MINGLE HMAC KEY>'}
+    params = { :user => { :admin => false, :activated => false } }
+
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    if uri.scheme == 'https'
+      http.use_ssl = true
+      if [:skip_ssl_verify]
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      end
+    end
+    body = params.to_json
+
+    request = Net::HTTP::Put.new(uri.request_uri)
+    request.body = body
+
+    request['Content-Type'] = 'application/json'
+    request['Content-Length'] = body.bytesize
+
+
+    if keys[:access_key_id]
+      ApiAuth.sign!(request, keys[:access_key_id], keys[:access_secret_key])
+    end
+
+    response = http.request(request)
+
+    if response.code.to_i > 300
+        raise StandardError, <<-ERROR
+        Request URL: #{url}
+        Response: #{response.code}
+        Response Message: #{response.message}
+        Response Headers: #{response.to_hash.inspect}
+        Response Body: #{response.body}
+        ERROR
+    end
+
+    return response
+  end
 end
 
-update_each_user(keys)
+# def update_each_user(user_arr, current_user_id)
+#     user_arr.delete_if{|x| x == current_user_id}
+#     user_arr.each do |user|
+#       url = 'https://sarahh.mingle-api.thoughtworks.com/api/v2/users/' + user + '.xml'
+#       keys = {:access_key_id => 'admin', :access_secret_key => 'kRSr/Tv++vQDWyrwh8eGJLS3JvSFDr41AQcHq/sPq0c='}
+#       params = { :user => { :activated => false } }
+#       make_request(url, 'put', params, keys)
+#     end
+# end
+
+response = http_get(all_users_url, keys)
+user_id_array = parse(response)
+http_put(user_id_array)
