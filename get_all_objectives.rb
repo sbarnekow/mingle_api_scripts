19da5ad74d18fbfa3fa15a3c9afb7fefc0c33784
Sarah Barnekow <mellamopepe@gmail.com>
2014-11-24 15:20:43 +0200

    adding files for objectives scripts

---
+++ get_all_objectives.rb
@@ -0,0 +1,44 @@
+require 'net/http'
+require 'net/https'
+require 'time'
+require 'api-auth'
+require 'json'
+
+URL = 'https://<instance name>.mingle-api.thoughtworks.com/api/v2/programs/test_program/plan/objectives.xml'
+OPTIONS = {:access_key_id => '<MINGLE USERNAME>', :access_secret_key => '<MINGLE HMAC KEY>'}
+
+def http_get(url, options={})
+    uri = URI.parse(url)
+    
+    http = Net::HTTP.new(uri.host, uri.port)
+    
+    if uri.scheme == 'https'
+      http.use_ssl = true
+      if options[:skip_ssl_verify]
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      end
+    end
+
+    request = Net::HTTP::Get.new(uri.request_uri)
+
+
+    if options[:access_key_id]
+      ApiAuth.sign!(request, options[:access_key_id], options[:access_secret_key])
+    end
+
+    response = http.request(request)
+
+    objectives = response.body
+
+    puts objectives
+
+    if response.code.to_i > 300
+      raise UnexpectedResponseError, <<-ERROR
+      \nRequest URL: #{url}
+      Response: #{response.code} #{response.message}
+      Response Headers: #{response.to_hash.inspect}\nResponse Body: #{response.body}"
+      ERROR
+  end
+end
+
+http_get(URL, OPTIONS)
