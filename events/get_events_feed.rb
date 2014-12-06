11206522d501496d5cddb79cf0ca9cccd8761eb3
Sarah Barnekow <mellamopepe@gmail.com>
2014-12-05 19:34:34 -0600

    cleaned up events example code

--- events/get_events_feed.rb
+++ events/get_events_feed.rb
@@ -8,38 +8,27 @@ URL = 'https://<instance name>.mingle-api.thoughtworks.com/api/v2/projects/test_
 OPTIONS = {:access_key_id => '<MINGLE USERNAME>', :access_secret_key => '<MINGLE HMAC KEY>'}
 
 def http_get(url, options={})
-    uri = URI.parse(url)
-    
-    http = Net::HTTP.new(uri.host, uri.port)
-    
-    if uri.scheme == 'https'
-      http.use_ssl = true
-      if options[:skip_ssl_verify]
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      end
-    end
+  uri = URI.parse(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  request = Net::HTTP::Get.new(uri.request_uri)
+  
+  ApiAuth.sign!(request, options[:access_key_id], options[:access_secret_key])
 
-    request = Net::HTTP::Get.new(uri.request_uri)
+  response = http.request(request)
+  events = response.body
 
-    if options[:access_key_id]
-      ApiAuth.sign!(request, options[:access_key_id], options[:access_secret_key])
-    end
-
-    response = http.request(request)
-
-    all_events = response.body
-
-    puts events
-
-     if response.code.to_i > 300
-      raise StandardError, <<-ERROR
-      Request URL: #{url}
-      Response: #{response.code}
-      Response Message: #{response.message}
-      Response Headers: #{response.to_hash.inspect}
-      Response Body: #{response.body}
-      ERROR
+  if response.code.to_i > 300
+    raise StandardError, <<-ERROR
+    Request URL: #{url}
+    Response: #{response.code}
+    Response Message: #{response.message}
+    Response Headers: #{response.to_hash.inspect}
+    Response Body: #{response.body}
+    ERROR
   end
+
+  events 
 end
 
 http_get(URL, OPTIONS)
