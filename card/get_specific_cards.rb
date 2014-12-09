f408d94a4622d642ab29b3690edcb9198440d5fa
Sarah Barnekow <mellamopepe@gmail.com>
2014-12-09 14:00:00 +0100

    added extra param to url retro

--- card/get_specific_cards.rb
+++ card/get_specific_cards.rb
@@ -3,7 +3,7 @@ require 'time'
 require 'api-auth'
 require 'json'
 
-URL = 'https://<instance name>.mingle-api.thoughtworks.com/api/v2/projects/test_project/cards.xml'
+URL = 'https://<instance name>.mingle-api.thoughtworks.com/api/v2/projects/test_project/cards/1.xml'
 OPTIONS = {:access_key_id => '<MINGLE USERNAME>', :access_secret_key => '<MINGLE HMAC KEY>'}
 
 def http_get(url, options={})
